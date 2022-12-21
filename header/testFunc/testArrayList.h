c256a28712a17cefb5aa0bdd9c152d9ef684fea5
JamesNULLiu <jamesnulliu@outlook.com>
2022-12-21 10:27:17 +0800

    .

--- header/testFunc/testArrayList.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <iostream>
-void array_test1(int N, const int k, const int m, std::ostream* out = &std::cout);
-int array_test2(int N, const int k, const int m, std::ostream* out = &std::cout);
-void outputResultofTest2(int N = 100);
--- header/testFunc/testDChain.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <iostream>
-void circle_test1(int N, const int k, const int m, std::ostream* out = &std::cout);
-void circle_test2(int N, const int k, const int m, std::ostream* out = &std::cout);
