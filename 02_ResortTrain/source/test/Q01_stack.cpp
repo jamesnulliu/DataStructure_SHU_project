764f2715b88dd4db159f573ee9901ed239197204
pedestrian0626 <670102181@qq.com>
2022-12-24 16:52:19 +0800

    Q1 completed

--- 02_ResortTrain/source/test/Q01_stack.cpp
+++ 02_ResortTrain/source/test/Q01_stack.cpp
@@ -28,12 +28,41 @@ string Q01_stack(const vector<int>& target)
     for (size_t i = 1; i <= input.size(); ++i) { input[i - 1] = static_cast<int>(i); }
     // @}
     arrayStack<int> stack;
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    while (!stack.empty()) {
-        cout << stack.top();
-        stack.pop();
+    int d, No;//d is target ,No is initial sequence
+    No = 1;
+    for (int i = 0; i < target.size(); i++)
+    {
+        d = target[i];
+        if (!stack.empty()&&stack.top()==d)
+        {
+            cout << "第" << stack.top() << "号从辅轨道进入主轨道右边。" << endl;
+            stack.pop();
+        }
+        else if(No<=d)
+        {
+            while (No<=target.size() && No < d)
+            {
+                cout << "第" << No << "号从主轨道左边进入辅轨道。" << endl;
+                stack.push(No++);
+            }
+            if (No==d)
+            {
+                cout << "第" << No << "号从主轨道左边进入主轨道右边。" << endl;
+                No++;
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+    if (stack.empty())
+    {
+        cout << "调度完成" << endl;
+    }
+    else
+    {
+        cout<<"调度无法完成" << endl;
     }
     return "test";
 }
