8508c60582f8929b03b5d985e2e92a6b24cde9d7
jamesNULLiu <jamesnulliu@outlook.com>
2023-02-22 19:08:29 +0800

    question

--- 04_BinaryTree/Solution/Q_01_solve.cpp
+++ 04_BinaryTree/Solution/Q_01_solve.cpp
@@ -1,8 +1,75 @@
 #include "Solution.h"
+#include <string>
+#include <iostream>
+using namespace std;
 
-void Q_01_refresh() {
-    system("cls");
+struct node
+{
+    int data;
+    node *left;
+    node *right;
+};
+
+void write1(node *root, string &str, int &i)
+{
+    if(str[i]=='#'){
+        delete root;
+        return;
+    }
+    root->data=str[i];
+    root->left=new node;
+    root->right=new node;
+    write1(root->left,str,++i);
+    write1(root->right,str,++i);
 }
 
-void Q_01_solve() {
+int i = 0;
+
+void write(node *root, string &str)
+{
+
+    if (str[i] == '#')
+    {
+        delete root;
+        return;
+    }
+    root->data = str[i];
+    ++i;
+    if (str[i + 1] != '#')
+    {
+        root->left = new node;
+        write(root->left, str);
+    }
+    ++i;
+    if (str[i + 2] != '#')
+    {
+        root->right = new node;
+        write(root->right, str);
+    }
+}
+
+node *root = new node;
+void Q_01_refresh()
+{
+    system("cls");
+    string str;
+    cout << "Please input the tree: ";
+    cin >> str;
+    write(root, str);
+}
+
+void reverse(node *root)
+{
+    if (root->left && root->right == nullptr)
+        return;
+    reverse(root->left);
+    reverse(root->right);
+    node *t = root->left;
+    root->left = root->right;
+    root->right = t;
+}
+
+void Q_01_solve()
+{
+    reverse(root);
 }
